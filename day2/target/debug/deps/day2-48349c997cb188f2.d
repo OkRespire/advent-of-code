394151c7ab6196aca9b0d27ea42af6a9b195ef51
Rohane Pranlal <148709418+OkRespire@users.noreply.github.com.>
2024-12-03 20:51:25 +0000

    fixed day2. turns out day2 code was correct, but text was not. Tried to salvage the code but i couldnt completely get my code back, however this code still works. also removed .zig

---
+++ day2/target/debug/deps/day2-48349c997cb188f2.d
@@ -0,0 +1,5 @@
+C:\Users\rohan\Documents\stuffs\advent-of-code\day2\target\debug\deps\libday2-48349c997cb188f2.rmeta: src/main.rs
+
+C:\Users\rohan\Documents\stuffs\advent-of-code\day2\target\debug\deps\day2-48349c997cb188f2.d: src/main.rs
+
+src/main.rs:
